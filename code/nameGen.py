45fd7afdcd3f745a98c29dee9f4731d2f47e5784
Edern76 <gawein.le@live.fr>
2017-01-24 21:32:18 +0100

    Continued work on name generator
    Still very WIP

--- code/nameGen.py
+++ code/nameGen.py
@@ -1,3 +1,32 @@
 from random import randint
+from constants import *
 
+
+def humanLike(length = randint(6, 12)):
+    #Generates pronouncable, human sounding like names (in theory)
+    finalName = []
+    for loop in range(length):
+        if loop == 0:
+            finalName[loop] = alphabet[randint(0, len(alphabet)-1)]
+        else:
+            previousChar = finalName[loop - 1]
+            allowedLetters = []
+            if previousChar == "c":
+                extrasAllowed = ["h", "r", "l"]
+                allowedLetters.extend(extrasAllowed)
+                allowedLetters.extend(vowels)
+            elif previousChar == "g":
+                extrasAllowed = ["r", "l"]
+                allowedLetters.extend(extrasAllowed)
+                allowedLetters.extend(vowels)
+            elif previousChar == "k":
+                extrasAllowed = ["r"]
+                allowedLetters.extend(extrasAllowed)
+                allowedLetters.extend(vowels)
+            elif previousChar == "s":
+                if (not loop - 1 == 0) and (finalName[loop-2] != "s"):
+                    extrasAllowed = ["s"]
+                    allowedLetters.extend(extrasAllowed)
+                allowedLetters.extend(vowels)
+            
 #WIP
