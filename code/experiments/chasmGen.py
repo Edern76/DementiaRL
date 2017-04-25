826def37e3f5c838308645ca6dd46324f2d56056
Malan-Tai <malantai99@gmail.com>
2017-04-25 21:24:24 +0200

    Started implementing chasm generation

---
+++ code/experiments/chasmGen.py
@@ -0,0 +1,397 @@
+import tdl, colors, copy, pdb, traceback, os, sys, time
+from random import *
+from colors import darker_sepia
+from custom_except import *
+
+
+WIDTH, HEIGHT, LIMIT = 150, 80, 20
+MAP_WIDTH, MAP_HEIGHT = 140, 60
+MID_MAP_WIDTH, MID_MAP_HEIGHT = MAP_WIDTH//2, MAP_HEIGHT//2
+MID_WIDTH, MID_HEIGHT = int(WIDTH/2), int(HEIGHT/2)
+
+CHANCE_TO_START_ALIVE = 55
+DEATH_LIMIT = 3
+BIRTH_LIMIT = 4
+STEPS_NUMBER = 2
+MIN_ROOM_SIZE = 6
+
+emptyTiles = [] #List of tuples of the coordinates of emptytiles not yet processed by the floodfill algorithm
+rooms = []
+roomTiles = []
+tunnelTiles = []
+visuTiles = []
+visuEdges = []
+confTiles = []
+dispEmpty = False
+dispDebug = True
+state = "base"
+
+curRoomIndex = 0
+
+sys.setrecursionlimit(3000)
+
+if __name__ == '__main__':
+    root = tdl.init(WIDTH, HEIGHT, 'Dementia')
+    
+class Tile:
+    def __init__(self, blocked):
+        self.blocked = blocked
+        self.indestructible = False
+        self.belongsTo = []
+        self.fg = colors.dark_grey
+        self.bg = colors.black
+        self.chasm = True
+        
+    def setIndestructible(self):
+        self.blocked = True
+        self.indestructible = True
+        
+    def open(self):
+        if not self.indestructible:
+            self.blocked = False
+            return True
+        else:
+            return False
+    
+    def close(self):
+        if not self.blocked:
+            self.blocked = True
+    
+    def addOwner(self, toAdd):
+        if not toAdd in self.belongsTo:
+            if self.belongsTo:
+                otherOwners = list(self.belongsTo)
+            else:
+                otherOwners = []
+            self.belongsTo.append(toAdd)
+            print(otherOwners)
+            return otherOwners
+    
+    def returnOtherOwners(self, base):
+        newList = list(self.belongsTo)
+        newList.remove(base)
+        return newList
+
+        
+class Room:
+    def __init__(self, tiles, borders = []):
+        self.tiles = tiles
+        self.borders = borders
+        rooms.append(self)
+        self.contestedTiles = []
+        self.collidingRooms = []
+        self.connectedRooms = []
+        self.protect = False
+        self.mainRoom = False
+        self.reachableFromMainRoom = False
+        
+    def remove(self):
+        for (x,y) in self.tiles:
+            closeTile(x, y, myMap)
+        rooms.remove(self)
+        del self
+        
+    def claimTile(self, x, y):
+        if (x,y) in self.tiles or (x,y) in self.borders:
+            conflict = myMap[x][y].addOwner(self)
+            if conflict:
+                print("CONFLICT")
+                self.contestedTiles.append((x,y))
+                for contester in conflict:
+                    if not contester in self.collidingRooms:
+                        self.collidingRooms.append(contester)
+        else:
+            raise IllegalTileInvasion("At {} {}".format(x, y))
+        
+    def claimBorders(self):
+        for (x, y) in self.borders:
+            self.claimTile(x, y)
+            
+    def mergeWith(self, other, arbitraryTiles = []):
+        self.protect = True
+        if not other.protect:
+            if self in self.collidingRooms:
+                self.collidingRooms.remove(self)
+                print("REMOVED SELF FROM COLLROOMS")
+            for (x,y) in other.tiles:
+                if not (x,y) in self.tiles:
+                    self.tiles.append((x,y))
+    
+            for (x,y) in arbitraryTiles:
+                if not (x,y) in self.tiles:
+                    self.tiles.append((x,y))
+            
+            for (x,y) in other.borders:
+                if not (x,y) in self.borders:
+                    self.borders.append((x,y))
+    
+            if other in rooms:
+                rooms.remove(other)
+            else:
+                print("Other room not in rooms")
+            if other in self.collidingRooms:
+                self.collidingRooms.remove(other)
+            else:
+                print("Other room not in colliding rooms")
+            del other
+        else:
+            print("OTHER ROOM IS FUCKING PROTECTED, DO NOT MERGE")
+            
+    def setReachable(self):
+        if not self.reachableFromMainRoom:
+            self.reachableFromMainRoom = True
+            for room in self.connectedRooms:
+                room.setReachable()
+
+
+class Rectangle:
+    def __init__(self, x, y, w, h):
+        self.x1 = x
+        self.y1 = y
+        self.x2 = x + w
+        self.y2 = y + h
+        self.tiles = []
+        
+    def center(self):
+        center_x = (self.x1 + self.x2) // 2
+        center_y = (self.y1 + self.y2) // 2
+        return (center_x, center_y)
+ 
+    def intersect(self, other):
+        return (self.x1 <= other.x2 and self.x2 >= other.x1 and
+                self.y1 <= other.y2 and self.y2 >= other.y1)
+        
+def createRoom(room):
+    global myMap, roomTiles
+    for x in range(room.x1 + 1, room.x2):
+        for y in range(room.y1 + 1, room.y2):
+            myMap[x][y].blocked = False
+            myMap[x][y].bg = colors.green
+            room.tiles.append(myMap[x][y])
+            roomTiles.append(myMap[x][y])
+            
+def createHorizontalTunnel(x1, x2, y):
+    global myMap, tunnelTiles
+    for x in range(min(x1, x2), max(x1, x2) + 1):
+        myMap[x][y].blocked = False
+        tunnelTiles.append(myMap[x][y])
+            
+def createVerticalTunnel(y1, y2, x):
+    global myMap, tunnelTiles
+    for y in range(min(y1, y2), max(y1, y2) + 1):
+        myMap[x][y].blocked = False
+        tunnelTiles.append(myMap[x][y])
+
+def unblockTunnels():
+    global myMap, tunnelTiles
+    for x in range(MAP_WIDTH):
+        for y in range(MAP_HEIGHT):
+            if myMap[x][y].chasm and myMap[x][y] in tunnelTiles:
+                myMap[x][y].chasm = False
+                myMap[x][y].fg = colors.lighter_grey
+                myMap[x][y].bg = colors.red
+    
+def makeMap():
+    global myMap, baseMap, rooms, roomTiles, tunnelTiles
+
+    myMap = [[Tile(blocked = True) for y in range(MAP_HEIGHT)]for x in range(MAP_WIDTH)] #Creates a rectangle of blocking tiles from the Tile class, aka walls. Each tile is accessed by myMap[x][y], where x and y are the coordinates of the tile.
+    rooms = []
+    roomTiles = []
+    tunnelTiles = []
+    numberRooms = 0
+    
+    for x in range(MAP_WIDTH):
+        myMap[x][0].setIndestructible()
+        removeFromEmptyTiles(x,0)
+        myMap[x][MAP_HEIGHT - 1].setIndestructible()
+        removeFromEmptyTiles(x, MAP_HEIGHT - 1)
+        for y in range(MAP_HEIGHT):
+            if not myMap[x][y].blocked and not (x,y) in emptyTiles:
+                emptyTiles.append((x,y))
+    for y in range(MAP_HEIGHT):
+        myMap[0][y].setIndestructible()
+        removeFromEmptyTiles(0, y)
+        myMap[MAP_WIDTH - 1][y].setIndestructible()
+        removeFromEmptyTiles(MAP_WIDTH - 1, y)
+
+    baseMap = list(copy.deepcopy(myMap))
+ 
+    for r in range(30):
+        w = randint(6, 10)
+        h = randint(6, 10)
+        x = randint(0, MAP_WIDTH-w-1)
+        y = randint(0, MAP_HEIGHT-h-1)
+        newRoom = Rectangle(x, y, w, h)
+        intersection = False
+        for otherRoom in rooms:
+            if newRoom.intersect(otherRoom):
+                intersection = True
+                break
+        if not intersection:
+            createRoom(newRoom)
+            (new_x, new_y) = newRoom.center()
+            if numberRooms != 0:
+                (previous_x, previous_y) = rooms[numberRooms-1].center()
+                if randint(0, 1):
+                    createHorizontalTunnel(previous_x, new_x, previous_y)
+                    createVerticalTunnel(previous_y, new_y, new_x)
+                else:
+                    createVerticalTunnel(previous_y, new_y, previous_x)
+                    createHorizontalTunnel(previous_x, new_x, new_y)
+            rooms.append(newRoom)
+            numberRooms += 1
+    
+    for x in range(1, MAP_WIDTH - 1):
+        for y in range(1, MAP_HEIGHT - 1):
+            if randint(0, 100) < CHANCE_TO_START_ALIVE:
+                myMap[x][y].fg = colors.lighter_grey
+                myMap[x][y].bg = colors.dark_sepia
+                myMap[x][y].chasm = False
+    for loop in range(STEPS_NUMBER):
+        myMap = doStep(myMap)
+    for tile in roomTiles:
+        tile.bg = colors.green
+    for tile in tunnelTiles:
+        tile.bg = colors.blue
+    unblockTunnels()
+
+myMap = [[]]
+baseMap = [[]]
+
+
+maps = [myMap, baseMap]
+mapIndex = 0
+
+def countNeighbours(mapToUse, startX, startY, stopAtFirst = False):
+    count = 0
+    found = False
+    for x in range(-1, 2):
+        for y in range(-1, 2):
+            if x == 0 and y == 0:
+                continue
+            else:
+                otherX = startX + x
+                otherY = startY + y
+                if not mapToUse[otherX][otherY].chasm:
+                    count += 1
+                    found = True
+                    if stopAtFirst:
+                        break
+        if stopAtFirst and found:
+            break
+    return count
+def removeFromEmptyTiles(x, y):
+    if (x,y) in emptyTiles:
+        emptyTiles.remove((x,y))
+
+def openTile(x, y, mapToUse):
+    if mapToUse[x][y].open() and not (x,y) in emptyTiles:
+        #emptyTiles.append((x,y))
+        pass
+
+def closeTile(x, y, mapToUse):
+    mapToUse[x][y].close()
+    #removeFromEmptyTiles(x,y)
+    
+def refreshEmptyTiles():
+    global emptyTiles
+    emptyTiles = []
+    for x in range(MAP_WIDTH):
+        for y in range(MAP_HEIGHT):
+            if not myMap[x][y].blocked:
+                emptyTiles.append((x,y))
+
+def doStep(oldMap):
+    newMap = list(copy.deepcopy(myMap))
+    for x in range(1, MAP_WIDTH - 1):
+        for y in range(1, MAP_HEIGHT - 1):
+            neighbours = countNeighbours(oldMap, x, y)
+            if not oldMap[x][y].chasm:
+                if neighbours < DEATH_LIMIT:
+                    newMap[x][y].fg = colors.dark_grey
+                    newMap[x][y].bg = colors.black
+                    newMap[x][y].chasm = True
+                else:
+                    newMap[x][y].fg = colors.lighter_grey
+                    newMap[x][y].bg = colors.dark_sepia
+                    newMap[x][y].chasm = False
+            else:
+                if neighbours > BIRTH_LIMIT:
+                    newMap[x][y].fg = colors.lighter_grey
+                    newMap[x][y].bg = colors.dark_sepia
+                    newMap[x][y].chasm = False
+                else:
+                    newMap[x][y].fg = colors.dark_grey
+                    newMap[x][y].bg = colors.black
+                    newMap[x][y].chasm = True
+    return newMap
+
+def drawCentered(cons = root , y = 1, text = "Lorem Ipsum", fg = None, bg = None):
+    xCentered = (WIDTH - len(text))//2
+    cons.draw_str(xCentered, y, text, fg, bg)
+
+
+        
+    
+def update(mapToUse):
+    root.clear()
+    try:
+        for x in range(MAP_WIDTH):
+            for y in range(MAP_HEIGHT):
+                if mapToUse[x][y].blocked:
+                    root.draw_char(x, y, char = '#', fg = mapToUse[x][y].fg, bg = mapToUse[x][y].bg)
+                else:
+                    root.draw_char(x, y, char = None, fg = mapToUse[x][y].fg, bg = mapToUse[x][y].bg)
+                    
+        #if dispEmpty:
+        #    for (x,y) in emptyTiles:
+        #        root.draw_char(x, y, char= ".", bg = colors.cyan)
+        #    print(len(emptyTiles))
+        
+    except IndexError:
+        traceback.print_exc()
+        #pdb.set_trace()
+        os._exit(-1)
+        
+    tdl.flush()
+    
+def getInput():
+    global mapIndex
+    key = tdl.event.key_wait()
+    actualKey = key.keychar.upper()
+    if actualKey == 'ENTER':
+        makeMap()
+    elif actualKey == 'SPACE':
+        print('CHANGING')
+        if mapIndex == 0:
+            mapIndex = 1
+        elif mapIndex == 1:
+            mapIndex = 0
+    elif actualKey == 'E':
+        global dispEmpty
+        dispEmpty = not dispEmpty
+    elif actualKey == 'A':
+        global dispDebug
+        dispDebug = not dispDebug
+    elif actualKey in ("LEFT", "RIGHT"):
+        global curRoomIndex
+        maxIndex = len(rooms) - 1
+        if actualKey == "LEFT":
+            curRoomIndex -= 1
+        else:
+            curRoomIndex += 1
+        if curRoomIndex < 0:
+            curRoomIndex = int(maxIndex)
+        if curRoomIndex > maxIndex:
+            curRoomIndex = 0
+
+
+        
+    
+    
+if __name__ == '__main__':
+    makeMap()
+    while not tdl.event.is_window_closed():
+        update(myMap)
+        #input = getInput()
+                
