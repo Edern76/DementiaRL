00f626f5a3e03b2f3014a2bba570efd293e16a53
Edern76 <gawein.le@live.fr>
2017-07-16 20:48:32 +0200

    Partially rewrote beginning of spellGen
    Realized my first spell generation idea was flawed from a game-balance
    point of view, so I decided to rewrite some stuff in the few lines I
    already wrote. Turns out this caused everything to break, so had to
    write a lot of not so useful lines to try and fix it.
    
    TL;DR : Spell generation hasn't progressed since last commit.

--- code/custom_except.py
+++ code/custom_except.py
@@ -12,3 +12,13 @@ class IllegalTileInvasion(Exception):
         warningToPrint = ("A room cannot claim a tile that doesn't belong to it. \n")
         finalMessage = warningToPrint + self.message
         return finalMessage
+
+class WrongElementTypeException(Exception):
+    def __init__(self, message):
+        self.message = message
+    def __str__(self):
+        warningToPrint = ("An EvenBetterList must only contain WeightedChoices \n")
+        if self.message is None:
+            self.message = ''
+        finalMessage = warningToPrint + self.message
+        return finalMessage
--- code/spellGen.py
+++ code/spellGen.py
@@ -1,8 +1,25 @@
-import copy
+import copy, inspect
+import code.custom_except
 from random import randint
 from code.constants import *
 
+SPELL_GEN_DEBUG = True
+testList = [2, 5]
 
+class WeightedChoice:
+    def __init__(self, name, prob):
+        self.name = name
+        self.prob = prob
+        
+        print("FUCKING INITIALIZED AS A {}".format(self.__class__.__name__))
+        
+    def __iter__(self):
+        print("Iterating")
+        return iter([self.name, self.prob]) #For some reason Python wants this class to be iterable, or else it won't put it into my custom list classes. So I made it iterable, though you probably don't want to use that as an iterator ever.
+    
+    def __str__(self):
+        return "{} : {} AS STRING".format(self.name, self.prob)
+    
 class SpellTemplate:
     def __init__(self):
         self.type = None
@@ -35,8 +52,53 @@ class BetterList(list):
         ind = randint(0, len(self) - 1)
         return self[ind]
 
-baseTypeList = BetterList("Attack", "Buff", "Heal")
-baseTargetList = BetterList("Select", "Self", "Closest", "Farthest")
+class EvenBetterList(list): #DON'T EVER MAKE THIS INHERIT FROM BETTERLIST IF YOU VALUE YOUR SANITY. (it's probably going to crash this way too though) (actually nvm it's a real pain that way too)
+    def __init__(self, *args):
+        selfList = []
+        savedArgs = locals()
+        for thing in savedArgs["args"]:
+            selfList.append(thing)
+            if SPELL_GEN_DEBUG:
+                print(thing)
+        list.__init__(self, selfList)
+        if SPELL_GEN_DEBUG:
+            print("Initialized EBL !")
+            for stuffThatShouldBeAWeightedChoiceAndNotAFuckingStringIHateYouPython in self:
+                print("PRINTING")
+                print(stuffThatShouldBeAWeightedChoiceAndNotAFuckingStringIHateYouPython)
+        for thing in self:
+            if not thing.__class__.__name__ == "WeightedChoice":
+                print(thing)
+                print(thing.__class__.__name__)
+                raise code.custom_except.WrongElementTypeException(thing)
+            elif SPELL_GEN_DEBUG:
+                print("{} : {}".format(thing.name, thing.prob))
+    
+    def removeFrom(self, element):
+        ind = self.index(element)
+        self.remove(self[ind])
+
+    def randFrom(self):
+        chances = []
+        for choice in self:
+            chances.append(choice.prob)
+        dice = randint(1, sum(chances))
+        runningSum = 0
+        choice = 0
+        for chance in chances:
+            runningSum += chance
+            if dice <= runningSum:
+                return choice
+            choice += 1
+        return self[choice]
+    
+    
+
+baseTypeList = BetterList("Attack", "Defense")
+
+target1 = WeightedChoice("Select", 25)
+
+baseTargetList = EvenBetterList(target1, WeightedChoice("Self", 25), WeightedChoice("Closest", 25), WeightedChoice("Farthest", 25))
 baseZoneList = BetterList("SingleTile", "Cross", "AOE")
 
 baseAttackList = BetterList("Fire", "Physical", "Poison")
@@ -52,6 +114,9 @@ def createEffect():
     attackList = copy.copy(baseAttackList)
     buffList = copy.copy(baseBuffList)
     healList = copy.copy(baseHealList)
+    
+    type = typeList.randFrom()
+    
     
     
     
